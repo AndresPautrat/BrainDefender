83440dcf948ba366dac37e586091a6f1f2538949
RenzoPL23 <renperez157@gmail.com>
2020-11-03 14:44:27 -0500

    Create Menu scene and UI

---
+++ Assets/Script/Menu/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void SceneGame()
+    {
+        SceneManager.LoadScene("Game");
+
+    }
+
+}
